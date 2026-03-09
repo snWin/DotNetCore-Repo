599d9e89cbc4bde916e7a17dba26956de573c763
Su Ni Win <suniwinn@gmail.com>
2026-03-09 21:15:01 +0800

    commit DTO

---
+++ NZWalk/NZWalk.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalk.API.Models.DTO
+{
+	public class RegionDto
+	{
+		//This Dto is subset of Domain Model
+		public Guid Id { get; set; }
+		public string Code { get; set; }
+		public string Name { get; set; } 
+		public string? RegionImageUrl { get; set; }
+	}
+}
